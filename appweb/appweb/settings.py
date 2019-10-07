0945c354d0e0a07ce7b4cefe3b428509c7832bdc
benazouz_ouilem <wilamben24@gmail.com>
2019-10-07 06:15:45 +0100

    test css

--- appweb/appweb/settings.py
+++ appweb/appweb/settings.py
@@ -146,7 +146,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 STATIC_ROOT  =  os.path.join(BASE_DIR, "static", "static_root")
 STATICFILES_DIRS = [
-    os.path.join(os.path.dirname(BASE_DIR), "static", "static_files"),
+    os.path.join(BASE_DIR, "static", "static_files"),
 ]
 #MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
 #STATIC_ROOT  =  os.path.join(os.path.dirname(BASE_DIR), "static", "static_root")
