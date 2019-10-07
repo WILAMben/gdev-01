a5688be9ef5d1f8fa60b32701c639c2946f314dd
benazouz_ouilem <wilamben24@gmail.com>
2019-10-07 06:59:36 +0100

    test css

--- appweb/appweb/settings.py
+++ appweb/appweb/settings.py
@@ -84,24 +84,24 @@ WSGI_APPLICATION = 'appweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG: 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+#if DEBUG: 
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        }
+#    }
+#else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'didaben_db',
+        'USER': 'didaben_db_user',
+        'PASSWORD': 'dev2019est',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'didaben_db',
-            'USER': 'didaben_db_user',
-            'PASSWORD': 'dev2019est',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 
 # Password validation
