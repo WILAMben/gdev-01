5e581f688d8c0ad6349aa49c1d8377779a503157
benazouz_ouilem <wilamben24@gmail.com>
2019-10-19 05:45:30 +0100

    bismilah

--- appweb/commande/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Commande, Ligne_commande
-
-admin.site.register(Commande)
-admin.site.register(Ligne_commande)
--- appweb/panier/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Panier_user
-
-# Register your models here.
-admin.site.register(Panier_user)
