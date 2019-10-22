f952ece402ec8ab3cac84579c535b50df884ba36
benazouz_ouilem <wilamben24@gmail.com>
2019-10-22 21:30:26 +0100

    ultimate testr

--- appweb/appweb/settings.py
+++ appweb/appweb/settings.py
@@ -140,12 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static_root/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
-STATIC_ROOT  = os.path.join(BASE_DIR,"static_root")
+STATIC_ROOT  = os.path.join(BASE_DIR,"static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static_files"),
