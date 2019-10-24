bc89cbd19518926a101ebd371f17543eaf8d9a42
benazouz_ouilem <wilamben24@gmail.com>
2019-10-24 01:42:56 +0100

    ultimate bellote

--- appweb/appweb/settings.py
+++ appweb/appweb/settings.py
@@ -148,7 +148,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 STATIC_ROOT  = os.path.join(BASE_DIR,"static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static"),
+    os.path.join(BASE_DIR,"static_files"),
 ]
 
 AUTH_USER_MODEL = 'utilisateur.User'
